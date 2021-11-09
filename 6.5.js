05ec1cb7f2e61eb7cacc5c01b90557c76ebef923
RL20 <harel_shalom@yahoo.com>
2021-11-09 22:37:22 +0200

    fix 6.5

--- 6.5.js
+++ 6.5.js
@@ -9,3 +9,33 @@ let arr2=[];
 // arr2=Array.from(Array(100).keys());
 arr2=Array.from({length: 100}, (_, i) => i + 1);
 console.log(arr2);
+//!3. Convert only values of an object into one array.
+let arrFromObjectValue=[]
+let data={0:"a", 1:"b", 2:"c"};
+for(key in data) {
+  if(data.hasOwnProperty(key)) {
+    arrFromObjectValue.push(data[key]) ;
+  }
+}
+console.log(`solution for 3-----${Array.isArray(arrFromObjectValue) }`);
+
+//!4 .Convert an array into one object
+// Object.assign({}, ['a','b','c']); // {0:"a", 1:"b", 2:"c"}
+let objConvertedFromArray={ ...['a', 'b', 'c'] };
+console.log(objConvertedFromArray);
+
+//!5. Find out if an array is an array
+arr3=[1, 2, 3];
+console.log(Array.isArray(arr3));
+//!6. Copy an array.
+// Old way
+//const cloneSheeps = sheeps.slice();
+
+// ES6 way
+//const cloneSheepsES6 = [...sheeps];
+let arr4=[1,2,3,4,5,6];
+let clonearr4=[...arr4];
+console.log(arr4 === clonearr4);
+// false --> it's pointing to a new memory space
+
+

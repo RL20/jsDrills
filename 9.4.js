9cfbabf95a4ea21ff192d25f0041553c441754ae
RL20 <harel_shalom@yahoo.com>
2021-11-14 14:30:27 +0200

    f

--- 9.4.js
+++ 9.4.js
@@ -1,10 +1,35 @@
-function steps(n){
-  
-  let step=Array(n).fill(' ');
-  for(i=0;step.indexOf(' ')!==-1;i++){
-    step[i]='#'
-    console.log(`'${step.join('')}'`);
+function steps(n) {
+  let step = Array(n).fill(" ");
+  for (i = 0; step.indexOf(" ") !== -1; i++) {
+    step[i] = "#";
+    console.log(`'${step.join("")}'`);
   }
 }
 steps(3);
 // !-----------------------------------------------------------
+console.log("\n\n");
+function steps2(n) {
+  let str = Array(n).fill(" ").join("");
+  let newStr = "";
+  let i = 0;
+  while (n > i) {
+    newStr += `#${str.slice(str.length - n + 1)}`;
+    console.log(`'${newStr}'`);
+    newStr = newStr.trim();
+    n--;
+  }
+}
+steps2(3);
+// !-----------------------------------------------------------
+function steps3(n) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    str += "#";
+    for (let j = 0; j < n - 1 - i; j++) {
+      str += " ";
+    }
+    console.log(`'${str}'`);
+    str = str.trim();
+  }
+}
+steps3(3);

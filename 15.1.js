04250497a9aa91213ad1307546ae896adb390426
RL20 <harel_shalom@yahoo.com>
2021-11-19 23:28:43 +0200

    Ex-15.1-get_sum-debug

---
+++ 15.1.js
@@ -0,0 +1,26 @@
+// function getSum(arr1, arr2){
+//   const sum = 0;
+//   for (let i=0; i < arr1.length; i++){
+//   sum += arr1[i];
+//   }
+//   for (let i=0; i < arr2.length; i++){
+//   sum += arr2[i];
+//   }
+//   }
+//   getSum([1,2,3][5,66,23]);
+
+//1-getSum undefined because comma missing between two arguments
+
+//2-sum set to const so it can't change and accumulate the result
+//?TypeError: Assignment to constant variable.
+//! fix the problem
+function getSum(arr1, arr2) {
+  let sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    sum += arr1[i];
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    sum += arr2[i];
+  }
+}
+getSum([1, 2, 3], [5, 66, 23]);

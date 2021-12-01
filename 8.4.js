da0f99449cec8a6d46522ba393e0de60b41fb50a
RL20 <harel_shalom@yahoo.com>
2021-12-01 09:44:14 +0200

    -

--- 8.4.js
+++ 8.4.js
@@ -1,15 +1,19 @@
-const array = ["Hello", "Good Day", "Your Welcome", "hotdog","hamburgers"];
-const newObj={};
-function letterAsKey(array){
-    let str=array.join('').replace(/\s/g,'').toLowerCase().split('');
-    str.forEach(char => {
-        newObj[char]=(newObj[char] &&  newObj[char]+1 )|| 1
-      });
-      return newObj;
-    }
+const array = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+const newObj = {};
+function letterAsKey(array) {
+  let str = array.join("").replace(/\s/g, "").toLowerCase().split("");
+  str.forEach((char) => {
+    newObj[char] = (newObj[char] && newObj[char] + 1) || 1;
+  });
+  return newObj;
+}
 
-    console.log(letterAsKey(array));
+console.log(letterAsKey(array));
 
-
-    let letterAsKey2=Array.from(array.join('').replace(/\s/g,'').toLowerCase()).reduce((acc,element) => ({...acc, [element]: (acc[element] || 0) + 1 }), {});
-    console.log(letterAsKey2);
+let letterAsKey2 = Array.from(
+  array.join("").replace(/\s/g, "").toLowerCase()
+).reduce(
+  (acc, element) => ({ ...acc, [element]: (acc[element] || 0) + 1 }),
+  {}
+);
+console.log(letterAsKey2);
